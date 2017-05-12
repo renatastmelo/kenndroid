35a522b5708271b6e0fe898da18f5a8aadffad6d
renatastmelo <renatastmelo@gmail.com>
2017-05-12 14:12:49 -0300

    layout da tela de cadastro de canil

--- src/app/src/main/java/renata/kenndroid/MainActivity.java
+++ src/app/src/main/java/renata/kenndroid/MainActivity.java
@@ -1,5 +1,6 @@
 package renata.kenndroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,13 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    public void onActivityCreated (Bundle savedInstanceState) {
+        final MainActivity activity = this;
+        View v = (View) findViewById(R.id.action_canil);
+        v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(activity, CanilActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -56,4 +60,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
