d5df8ef54559d46fc9bd338ff18deca94f2e327b
renatastmelo <renatastmelo@gmail.com>
2017-05-17 19:31:15 -0300

    Criação do layout de acasalamento e pequenas correções.

---
+++ src/app/src/main/java/renata/kenndroid/CadAcasalamento.java
@@ -0,0 +1,13 @@
+package renata.kenndroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadAcasalamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cad_acasalamento);
+    }
+}
