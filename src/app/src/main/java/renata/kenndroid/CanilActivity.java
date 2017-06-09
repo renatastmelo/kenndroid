ad7b1447680da582178370b9d457e7c1d63c5c3d
renatastmelo <renatastmelo@gmail.com>
2017-06-08 21:52:02 -0300

    Removidos arquivos de layout não utilizados. Teste de ScrollView.

--- src/app/src/main/java/renata/kenndroid/CanilActivity.java
+++ src/app/src/main/java/renata/kenndroid/CanilActivity.java
@@ -9,6 +9,6 @@ public class CanilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canil);
+        setContentView(R.layout.canil);
     }
 }
